1c7b26cca5aad90e34fc3a9064c94476ebb344d8
Vladimir <121644805+polyarn1y@users.noreply.github.com>
2024-11-02 09:30:08 +0300

    Update calc.php

--- lab4/site/calc.php
+++ lab4/site/calc.php
@@ -1,30 +1,69 @@
+<?php
+$result = null;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num1 = filter_input(INPUT_POST, 'num1', FILTER_VALIDATE_FLOAT);
+    $num2 = filter_input(INPUT_POST, 'num2', FILTER_VALIDATE_FLOAT);
+
+    if ($num1 === false || $num2 === false) {
+        echo "Введите корректные числа.";
+    } else {
+        switch ($_POST["operator"]) {
+            case '+':
+                $result = $num1 + $num2;
+                break;
+            case '-':
+                $result = $num1 - $num2;
+                break;
+            case '*':
+                $result = $num1 * $num2;
+                break;
+            case '/':
+                if ($num2 == 0) {
+                    echo "Ошибка: деление на ноль.";
+                } else {
+                    $result = $num1 / $num2;
+                }
+                break;
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Калькулятор</title>
-  <link rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Калькулятор</title>
 </head>
 <body>
-    <!-- Область основного контента -->
-    <form action=''>
-      <label>Число 1:</label>
-      <br>
-      <input name='num1' type='text'>
-      <br>
-      <label>Оператор: </label>
-      <br>
-      <input name='operator' type='text'>
-      <br>
-      <label>Число 2: </label>
-      <br>
-      <input name='num2' type='text'>
-      <br>
-      <br>
-      <input type='submit' value='Считать'>
-    </form>
-    <!-- Область основного контента -->
+
+<?php
+if ($result !== null) {
+    echo "<h2>Результат: $result</h2>";
+}
+?>
+
+<form action="index.php?id=calc" method="post">
+
+<p><label for="num1">Число 1</label><br>
+<input type="text" name="num1" id="num1" required></p>
+
+<p><label for="operator">Оператор</label><br>
+<select name="operator" id="operator">
+    <option value="+" selected >+</option>
+    <option value="-">-</option>
+    <option value="*">*</option>
+    <option value="/">/</option>
+</select></p>
+
+<p><label for="num2">Число 2</label><br>
+<input type="text" name="num2" id="num2" required></p>
+
+<button type="submit">Считать!</button>
+
+</form>
+
 </body>
 </html>
