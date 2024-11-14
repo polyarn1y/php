a32a7d6e80d4f2d41e0268f3fc97d4442954efd5
Vladimir <121644805+polyarn1y@users.noreply.github.com>
2024-11-14 15:45:38 +0300

    Update savepage.inc.php

--- lab6/session/savepage.inc.php
+++ lab6/session/savepage.inc.php
@@ -1,10 +1,17 @@
-<?php
-// Код для всех страниц - сохранение информации о посещенных страницах
-if (!isset($_SESSION['visitedPages'])) {
-    $_SESSION['visitedPages'] = [];
-}
-
-$current_page = $_SERVER['REQUEST_URI'];
-$parsed_url = parse_url($current_page, PHP_URL_PATH);
-$_SESSION['visitedPages'][] = $parsed_url;
+<?php
+
+if (!isset($_SESSION['visitedPages'])) {
+
+    $_SESSION['visitedPages'] = [];
+
+}
+
+
+
+$current_page = $_SERVER['REQUEST_URI'];
+
+$parsed_url = parse_url($current_page, PHP_URL_PATH);
+
+$_SESSION['visitedPages'][] = $parsed_url;
+
 ?>
