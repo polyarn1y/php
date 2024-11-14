d152b3cab7d348e7c355baa480a1c50e181fec78
Vladimir <121644805+polyarn1y@users.noreply.github.com>
2024-11-14 15:45:32 +0300

    Update visited.inc.php

--- lab6/session/visited.inc.php
+++ lab6/session/visited.inc.php
@@ -1,9 +1,15 @@
-<?php
-// Код для всех страниц - вывод информации о посещенных страницах
-
-echo "<ul>" ."<h2>Список посещённых страниц</h2>"."<ol>";
-foreach ($_SESSION['visitedPages'] as $page) {
-    echo "<li>" . htmlspecialchars($page) . "</li>";
-}
-echo "</ul>" . "</ol>";
+<?php
+
+
+
+echo "<ul>" ."<h2>Список посещённых страниц</h2>"."<ol>";
+
+foreach ($_SESSION['visitedPages'] as $page) {
+
+    echo "<li>" . htmlspecialchars($page) . "</li>";
+
+}
+
+echo "</ul>" . "</ol>";
+
 ?>
